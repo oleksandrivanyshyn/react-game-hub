e4e25100c756285762f440489ac76dd04f47c62c
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-03-01 22:14:25 +0200

    feat: build Color mode switch component and integrate

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,11 @@
-import { HStack, Text, Image } from '@chakra-ui/react';
+import { HStack, Image } from '@chakra-ui/react';
 import logo from '../assets/logo.webp';
+import ColorModeSwitch from './ColorModeSwitch.tsx';
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} boxSize="60px"></Image>
-      <Text>NavBar</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
