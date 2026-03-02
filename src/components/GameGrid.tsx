78c13d56515856bcc2436ad19080aa78475bfac3
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-03-02 17:26:43 +0200

    refactor: simplify error handling and remove unnecessary fragment in GameGrid component

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -11,23 +11,21 @@ interface GameGridProps {
 const GameGrid = ({ gameQuery }: GameGridProps) => {
   const { data: games, error, isLoading } = useGames(gameQuery);
   const skeletons = Array.from({ length: 15 }, (_, i) => i);
+  if (error) return <Text>{error}</Text>;
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={6}>
-        {isLoading &&
-          skeletons.map((s) => (
-            <GameCardContainer key={s}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-        {games.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game}></GameCard>
+    <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={6}>
+      {isLoading &&
+        skeletons.map((s) => (
+          <GameCardContainer key={s}>
+            <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {games.map((game) => (
+        <GameCardContainer key={game.id}>
+          <GameCard game={game}></GameCard>
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 export default GameGrid;
