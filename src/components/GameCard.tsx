93ef2af77016a900ba44ab40f78b9274fce1e13d
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-03-02 12:37:10 +0200

    feat: optimize game card images with cropped URLs

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,13 +2,14 @@ import type { Game } from '../hooks/useGames.ts';
 import { Card, CardBody, Heading, HStack, Image } from '@chakra-ui/react';
 import PlatformIconList from './PlatformIconList.tsx';
 import CriticScore from './CriticScore.tsx';
+import getCroppedImageUrl from '../services/image-url.ts';
 interface GameCardProps {
   game: Game;
 }
 const GameCard = ({ game }: GameCardProps) => {
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent={'space-between'}>
