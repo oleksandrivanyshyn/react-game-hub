8093a14603cf22bb5a4963073f100a73457d76fb
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-03-02 12:16:57 +0200

    feat: display platform icons on game cards

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,40 @@
+import type { Platform } from '../hooks/useGames.ts';
+import { HStack, Icon } from '@chakra-ui/react';
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from 'react-icons/fa';
+
+import { MdPhoneIphone } from 'react-icons/md';
+import { SiNintendo } from 'react-icons/si';
+import { BsGlobe } from 'react-icons/bs';
+import type { IconType } from 'react-icons';
+
+interface PlatformIconListProps {
+  platforms: Platform[];
+}
+const PlatformIconList = ({ platforms }: PlatformIconListProps) => {
+  const iconMap: Record<string, IconType> = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color="gray.500" />
+      ))}
+    </HStack>
+  );
+};
+export default PlatformIconList;
