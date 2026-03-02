a3c6ae866ee702098bd8c2fc1a262d2855d793fd
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-03-02 12:26:23 +0200

    feat: display critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,14 @@
+import { Badge } from '@chakra-ui/react';
+
+interface CriticScoreProps {
+  score: number;
+}
+const CriticScore = ({ score }: CriticScoreProps) => {
+  const color = score > 75 ? 'green' : score > 60 ? 'yellow' : 'red';
+  return (
+    <Badge fontStyle="14px" paddingX={2} borderRadius="4px" colorScheme={color}>
+      {score}
+    </Badge>
+  );
+};
+export default CriticScore;
