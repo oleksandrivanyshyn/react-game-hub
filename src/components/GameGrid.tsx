bbd88970ac7593e2cbcd0fa8331c5b04c06b8678
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-03-02 11:13:27 +0200

    feat: building game cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,14 +1,17 @@
-import { Text } from '@chakra-ui/react';
+import { SimpleGrid, Text } from '@chakra-ui/react';
 import useGames from '../hooks/useGames.ts';
+import GameCard from './GameCard.tsx';
 
 const GameGrid = () => {
   const { games, error } = useGames();
   return (
     <>
       {error && <Text>{error}</Text>}
-      {games.map((game) => (
-        <Text key={game.id}>{game.name}</Text>
-      ))}
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={10}>
+        {games.map((game) => (
+          <GameCard key={game.id} game={game}></GameCard>
+        ))}
+      </SimpleGrid>
     </>
   );
 };
