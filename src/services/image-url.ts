42d02414c7d135ba8b0addba8aa6d021c0c9572d
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-03-02 16:51:17 +0200

    feat: implement searching

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,5 @@
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return null;
+  if (!url) return undefined;
   const target = 'media/';
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
