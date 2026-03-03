8ac9b780a5884cfcd6a5837b0b252416e6cdc844
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-03-03 13:03:15 +0200

    chore: integrate react-query

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import './index.css';
 import App from './App.tsx';
 import theme from './theme.ts';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ChakraProvider>
   </StrictMode>,
 );
