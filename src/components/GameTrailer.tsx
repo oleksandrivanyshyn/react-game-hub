cc5458600380618544c53b77304ae73992505115
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-03-03 17:30:58 +0200

    feat: add GameTrailer component

---
+++ src/components/GameTrailer.tsx
@@ -0,0 +1,16 @@
+import useTrailers from '../hooks/useTrailers.ts';
+
+interface GameTrailerProps {
+  gameId: number;
+}
+const GameTrailer = ({ gameId }: GameTrailerProps) => {
+  const { data: trailers, error, isLoading } = useTrailers(gameId);
+  if (isLoading) return null;
+  if (error) throw error;
+  const first = trailers?.results[0];
+  return first ? (
+    <video src={first.data[480]} poster={first.preview} controls />
+  ) : null;
+};
+
+export default GameTrailer;
---
+++ src/hooks/useTrailers.ts
@@ -0,0 +1,12 @@
+import { useQuery } from '@tanstack/react-query';
+import APIClient from '../services/api-client.ts';
+import type { Trailer } from '../entities/Trailer.ts';
+
+const useTrailers = (gameId: number) => {
+  const apiClient = new APIClient<Trailer>(`/games/${gameId}/movies`);
+  return useQuery({
+    queryKey: ['trailers', gameId],
+    queryFn: apiClient.getAll,
+  });
+};
+export default useTrailers;
