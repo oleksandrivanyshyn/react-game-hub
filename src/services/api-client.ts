a610594aac5eeae373d499830191cbd0ff66ab36
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-03-05 13:57:22 +0200

    chore: use environment variable for API key in api-client

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -8,7 +8,7 @@ export interface FetchResponse<T> {
 
 const axiosInstance = axios.create({
   baseURL: 'https://api.rawg.io/api',
-  params: { key: 'd9a3acae7928485490af35ae3e55f33e' },
+  params: { key: import.meta.env.VITE_RAWG_API_KEY },
 });
 class APIClient<T> {
   endpoint: string;
